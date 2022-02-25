43825aaf6013026c26338fbc9deede40ac5f6bf5
vjgaur <vijayendra.gaur@gmail.com>
2022-02-25 15:29:11 +0530

    implemented geocode

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { stringify } = require('querystring');
 const slugify = require('slugify');
-
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -107,6 +107,25 @@ BootcampSchema.pre('save', function (next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 });
+
+//Geocode and create location field
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipCode,
+        country: loc[0].countryCode
+    }
+    //Do not save address in DB
+    this.address = undefined;
+    next();
+})
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
 
