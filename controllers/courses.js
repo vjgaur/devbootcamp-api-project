065c98a5e0a55882d502b25167f16d22185855ea
vjgaur <vijayendra.gaur@gmail.com>
2022-03-14 17:38:53 +0530

    implemented course update/delete api

--- controllers/courses.js
+++ controllers/courses.js
@@ -68,16 +68,6 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
       );
     }
   
-    // Make sure user is bootcamp owner
-    // if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-    //   return next(
-    //     new ErrorResponse(
-    //       `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
-    //       401
-    //     )
-    //   ); 
-    // }
-  
     const course = await Course.create(req.body);
   
     res.status(200).json({
@@ -85,4 +75,51 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
       data: course
     });
   });
+
+  
+// @desc      Update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+  
+    if (!course) {
+      return next(
+        new ErrorResponse(`No course with the id of ${req.params.id}`),
+        404
+      );
+    }
+  
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+  
+    res.status(200).json({
+      success: true,
+      data: course
+    });
+});
+  
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+  
+    if (!course) {
+      return next(
+        new ErrorResponse(`No course with the id of ${req.params.id}`),
+        404
+      );
+    }
+  
+    await course.remove();
+    
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  });
+  
   
--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,9 @@ const express = require('express');
 const {
     getCourses,
     getCourse,
-    addCourse
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({mergeParams:true});
@@ -11,8 +13,12 @@ router
     .route('/')
     .get(getCourses)
     .post(addCourse);
-    
-router.route('/:id').get(getCourse);
+
+router
+    .route('/:id')
+    .get(getCourse)
+    .put(updateCourse)
+    .delete(deleteCourse);
 
 
 module.exports = router;
