7dc0cb59308ae79c78c4ad8d589b42d0e0d8395f
vjgaur <vijayendra.gaur@gmail.com>
2022-03-14 12:40:55 +0530

    added bootcamp course model and seeded

--- seeder.js
+++ seeder.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 //Load models
 const Bootcamp = require('./models/Bootcamp');
-
+const Course = require('./models/Course');
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -17,11 +17,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data Imported...'.green.inverse)
         process.exit();
     } catch (error) {
@@ -33,6 +35,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data deleted...'.red.inverse)
         process.exit();
     } catch (error) {
