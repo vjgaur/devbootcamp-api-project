3557846265abba4a22ee27301a21829d78ca2b8d
vjgaur <vijayendra.gaur@gmail.com>
2022-03-15 12:43:20 +0530

    implemented user registration api

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 //File uploading
 app.use(fileUpload());
@@ -28,6 +29,8 @@ app.use(express.static(path.join(__dirname,'public')));
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+
 
 app.use(errorHandler);
 
