dfeae26f653d59ad7a304f46086e38eb96992dac
vjgaur <vijayendra.gaur@gmail.com>
2022-03-19 16:35:38 +0530

    created CURD admin operation api

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 //File uploading
 app.use(fileUpload());
@@ -31,6 +32,8 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 app.use(cookieParser());
 
 
